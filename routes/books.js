acb8af2c5695f3618db7b1120fcb42bbcc3f9522
SatYu26 <satyam.goyal@freightdeck.com>
2022-06-03 14:55:06 +0530

    day 3

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/search", function(req, res, next) {
-  res.status(200).json({id: 1, name: "Harry Potter", author: "J.K. Rowling"});
+router.get('/add', function(req, res, next) {
+  res.render('books');
+});
+
+router.post("/add", function(req, res, next) {
+  console.info(req.body);
+  res.status(200).json({message: "Book Saved Successfuly!!"});
+  
+});
+
+router.get("/find/:id", function(req, res, next) {
+  const {id} = req.params;
+  console.info(id);
+  res.status(200).json({message: "Book Found!!"});
 });
 
 module.exports = router;
